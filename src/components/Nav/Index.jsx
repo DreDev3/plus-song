59a60d42e6fb554de8d897957a2fcfdcfc044948
Edson André <andreedson02@gmail.com>
2025-09-06 19:53:06 -0300

    adição do menu hamburguer

--- src/components/Nav/Index.jsx
+++ src/components/Nav/Index.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import Toggle from '../Toggle/Index';
 import './Index.css';
 export default function Nav() {
+  const [isShort, setIsShort] = useState();
   return (
     <header>
       <div className="logo">
@@ -10,12 +14,19 @@ export default function Nav() {
           <img src={`${import.meta.env.BASE_URL}Logo.png`} alt="Logo Plus Song" />
         </NavLink>
       </div>
-      <nav>
-        <NavLink to="/plus-song">Home</NavLink>
-        <NavLink to="/search">Buscar músicas</NavLink>
-        <NavLink to="/top-success">Top Sucessos</NavLink>
-        <NavLink to="/contact">Contatos</NavLink>
-        <NavLink to="/about">Sobre Nós</NavLink>
+      <a
+        className="hamburguer"
+        onClick={() => setIsShort(!isShort)}
+        aria-label="Menu"
+      >
+        <FontAwesomeIcon icon={isShort ? faTimes : faBars} />
+      </a>
+      <nav className={isShort ? "open" : ""} >
+        <NavLink to="/plus-song" onClick={() => setIsShort(false)}>Home</NavLink>
+        <NavLink to="/search" onClick={() => setIsShort(false)}>Buscar músicas</NavLink>
+        <NavLink to="/top-success" onClick={() => setIsShort(false)}>Top Sucessos</NavLink>
+        <NavLink to="/contact" onClick={() => setIsShort(false)}>Contatos</NavLink>
+        <NavLink to="/about" onClick={() => setIsShort(false)}>Sobre Nós</NavLink>
       </nav>
       <Toggle />
     </header>
